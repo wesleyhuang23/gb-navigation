d4492144aee96b7fcb1960b8502cef8f150a442a
Wesley <wesleyhuang23@gmail.com>
2017-06-28 16:57:18 -0700

    hovering over submenu will not hide it

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 (function hover(){
     var navItems = document.getElementsByClassName('nav-items');
     console.log(navItems[0].children[0].innerText);
-    
-
         navItems[0].children[0].onmouseenter = function(){
             var subNav = document.getElementsByClassName(navItems[0].children[0].innerText)[0];
             subNav.id = 'showSubNav';
+                subNav.onmouseenter = function(){
+                    subNav.id = 'showSubNav';
+                }
+                subNav.onmouseleave = function(){
+                    subNav.id = '';
+                }
         }
         navItems[0].children[0].onmouseleave = function(){
             var subNav = document.getElementById('showSubNav');
-            subNav.id = '';
+            if(subNav){
+                subNav.id = '';
+            }
         }
  
 })();
