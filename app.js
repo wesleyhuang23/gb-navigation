03900b6ee98c378e4fd0e6c00247cb85a9c7c081
Wesley <wesleyhuang23@gmail.com>
2017-07-06 16:03:28 -0700

    hover over enter working

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 (function hover(){
-    var navItems = document.getElementsByClassName('nav-items');
-    console.log(navItems[0].children.length);
+    var navItems = document.getElementsByClassName('nav-items')[0].children;
+    console.log(navItems);
 
-    for(var i = 0; i < navItems[0].children.length; i++){
-        console.log(navItems[0].children[i].children[0].className)
-        var item = navItems[0].children[i].children[0].className;
-        navItems[0].children[i].onmouseenter = function(){
-            console.log(item);
-            var subNav = document.getElementsByClassName(item)[0];
-            subNav.id = 'showSubNav';
-                subNav.onmouseenter = function(){
-                    subNav.id = 'showSubNav';
-                }
-                subNav.onmouseleave = function(){
-                    subNav.id = '';
-                }
+    for(var i = 0; i < navItems.length; i++){
+        navItems[i].onmouseenter = function(e){
+            console.log(e);
+            e.target.children[0].style.display = 'block';
         }
-        navItems[0].children[i].onmouseleave = function(){
-            var subNav = document.getElementById('showSubNav');
-            if(subNav){
-                subNav.id = '';
-            }
+        navItems[i].onmouseleave = function(e){
+            console.log(e);
+            e.target.children[0].style.display = 'none';
         }
-        console.log(navItems);
     }
+    console.log(navItems);
 })();
