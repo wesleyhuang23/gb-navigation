36c5db7af86a2d18583cce434232c5907000a6ea
Wesley <wesleyhuang23@gmail.com>
2017-07-07 09:11:54 -0700

    able to create list items

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-(function hover(){
+function hover(){
     var navItems = document.getElementsByClassName('nav-items')[0].children;
     console.log(navItems);
 
@@ -12,10 +12,12 @@
             e.target.children[0].style.display = 'none';
         }
     }
-})();
+}
+hover();
 
 function addItem(){
     var inputItem = document.createElement('input');
+    inputItem.className = 'subNavItem'
     var button = document.createElement('button');
     button.innerText = 'Remove Item';
     button.onclick = function(){
@@ -29,5 +31,36 @@ function addItem(){
 }
 
 function addNav(e){
-    console.log(e);
+    console.log(e.target.parentNode.children);
+    var children = e.target.parentNode.children;
+    var navName = document.getElementById('navName').value;
+    console.log(navName);
+    let subNav = [];
+    for(var i = 0; i < children.length; i++){
+        if(children[i].className === 'subNavItem'){
+            subNav.push(children[i]);
+        }
+    }
+    console.log(subNav)
+    createNav(navName, subNav);
+}
+
+function createNav(name, items){
+    console.log(name, items);
+    var outList = document.createElement('li');
+    outList.innerText = name;
+    var container = document.createElement('div');
+    container.className = name;
+    var innerList = document.createElement('ul');
+    for(var i = 0; i < items.length; i++){
+        var subItem = document.createElement('li');
+        subItem.innerHTML = items[i].value;
+        innerList.appendChild(subItem);
+    }
+    console.log(innerList);
+    container.appendChild(innerList);
+    outList.appendChild(container);
+    var navItems = document.getElementsByClassName('nav-items')[0];
+    navItems.appendChild(outList);
+    hover();
 }
